8a665a40c9965b1efb4896b66d5e1ca032cd294a
Yue Wu <yuw264@ucsd.edu>
2020-07-27 22:24:02 -0700

    Added placeholder 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function NotFound() {
+    return (
+        <Layout current="/404">
+            <h1>404 Not Found</h1>
+        </Layout>
+    )
+}
