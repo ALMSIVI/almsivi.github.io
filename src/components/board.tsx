6448656c69f8e42fc806f6d5628f1145c9a3bb28
Yue Wu <yuw264@ucsd.edu>
2020-07-31 22:13:26 -0700

    Responsive board component

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { css } from '@emotion/core'
+import styles from '../utils/styles'
 
 export default function Board({ color = 'aliceblue', width = 'auto', children }) {
     return (
@@ -10,6 +11,9 @@ export default function Board({ color = 'aliceblue', width = 'auto', children })
                 margin: 0.5rem;
                 flex-basis: ${width};
                 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                @media (max-width: ${styles.mobileBreakpoint}) {
+                    flex-basis: 100%;
+                }
             `}
         >
             {children}
